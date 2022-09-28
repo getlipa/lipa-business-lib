2b7dfb42325fe6e4b9072f7ab3cad9da8338b507
Daniel Granhão <daniel.granhao@gmail.com>
2022-09-28 19:04:49 +0100

    Implement `get_balance()` + use descriptors

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,16 @@ mod errors;
 mod native_logger;
 mod secrets;
 mod signing;
+mod wallet;
 
-use crate::errors::{KeyDerivationError, MnemonicGenerationError, SigningError};
+use crate::errors::{KeyDerivationError, MnemonicGenerationError, SigningError, WalletError};
 use crate::native_logger::init_native_logger_once;
-use crate::secrets::{derive_keys, generate_mnemonic, KeyPair, LipaKeys};
+use crate::secrets::{derive_keys, generate_mnemonic, Descriptors, KeyPair, LipaKeys};
 use crate::signing::sign_message;
+use crate::wallet::{Config, Wallet};
 
 use bdk::bitcoin::Network;
+use bdk::Balance;
 use log::Level as LogLevel;
 
 include!(concat!(env!("OUT_DIR"), "/lipabusinesslib.uniffi.rs"));
