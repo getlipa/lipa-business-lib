4af27f5c6261b35b45d1b7945bdf096de4fc9646
Daniel Granhão <daniel.granhao@gmail.com>
2022-09-29 18:00:10 +0100

    Add `generate_keypair()` method

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@ mod secrets;
 mod signing;
 mod wallet;
 
-use crate::errors::{KeyDerivationError, MnemonicGenerationError, SigningError, WalletError};
+use crate::errors::{KeyDerivationError, KeyGenerationError, SigningError, WalletError};
 use crate::native_logger::init_native_logger_once;
-use crate::secrets::{derive_keys, generate_mnemonic, Descriptors, KeyPair, LipaKeys};
+use crate::secrets::{
+    derive_keys, generate_keypair, generate_mnemonic, Descriptors, KeyPair, LipaKeys,
+};
 use crate::signing::sign_message;
 use crate::wallet::{Config, Wallet};
 
