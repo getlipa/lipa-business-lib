822cd315c47946e096b723c21d8620ca312881ab
Daniel Granhão <daniel.granhao@gmail.com>
2022-12-15 17:10:32 +0000

    Implement `prepare_drain_tx`

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,13 +1,16 @@
 use crate::errors::{permanent_failure, runtime_error, LipaResult, MapToLipaError};
-use crate::RuntimeErrorCode::{ElectrumServiceUnavailable, GenericError, RemoteServiceUnavailable};
+use crate::RuntimeErrorCode::{
+    ElectrumServiceUnavailable, GenericError, NotEnoughFunds, RemoteServiceUnavailable,
+};
+use bdk::bitcoin::consensus::serialize;
 use bdk::bitcoin::{Address, Network};
-use bdk::blockchain::ElectrumBlockchain;
+use bdk::blockchain::{Blockchain, ElectrumBlockchain};
 use bdk::electrum_client::Client;
 use bdk::sled::Tree;
 use bdk::{Balance, SyncOptions};
 use std::path::Path;
 use std::str::FromStr;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, MutexGuard};
 
 pub struct Config {
     pub electrum_url: String,
@@ -64,16 +67,7 @@ impl Wallet {
     pub fn sync_balance(&self) -> LipaResult<Balance> {
         let wallet = self.wallet.lock().unwrap();
 
-        match wallet.sync(&self.blockchain, SyncOptions::default()) {
-            Ok(_) => {}
-            Err(e) => {
-                return match e {
-                    bdk::Error::Electrum(e) => Err(runtime_error(ElectrumServiceUnavailable, e)),
-                    bdk::Error::Sled(e) => Err(permanent_failure(e)),
-                    _ => Err(runtime_error(GenericError, "Failed to sync the BDK wallet")),
-                }
-            }
-        };
+        Self::sync_wallet(&wallet, &self.blockchain)?;
 
         let balance = wallet
             .get_balance()
@@ -95,8 +89,43 @@ impl Wallet {
         }
     }
 
-    pub fn prepare_drain_tx(&self, _addr: String) -> LipaResult<Tx> {
-        todo!()
+    pub fn prepare_drain_tx(&self, addr: String, confirm_in_blocks: u32) -> LipaResult<Tx> {
+        let address = Address::from_str(&addr)
+            .map_to_invalid_input("Failed to parse the provided bitcoin address")?;
+
+        let wallet = self.wallet.lock().unwrap();
+
+        Self::sync_wallet(&wallet, &self.blockchain)?;
+
+        let mut tx_builder = wallet.build_tx();
+
+        let fee_rate = self
+            .blockchain
+            .estimate_fee(confirm_in_blocks as usize)
+            .map_to_runtime_error(
+                ElectrumServiceUnavailable,
+                "Failed to estimate fee for drain tx",
+            )?;
+
+        tx_builder
+            .drain_wallet()
+            .drain_to(address.script_pubkey())
+            .fee_rate(fee_rate)
+            .enable_rbf();
+
+        let (psbt, tx_details) = tx_builder
+            .finish()
+            .map_to_runtime_error(NotEnoughFunds, "Failed to create PSBT")?;
+
+        let fee = tx_details.fee.unwrap(); // According to the docs, it's never None when using an Electrum backend
+        let tx = Tx {
+            id: tx_details.txid.to_string(),
+            blob: serialize(&psbt),
+            on_chain_fee_sat: fee,
+            output_sat: tx_details.sent - fee,
+        };
+
+        Ok(tx)
     }
 
     pub fn sign_and_broadcast_tx(
@@ -133,6 +162,23 @@ impl Wallet {
 
         Ok(address.to_string())
     }*/
+
+    fn sync_wallet(
+        wallet: &MutexGuard<bdk::Wallet<Tree>>,
+        blockchain: &ElectrumBlockchain,
+    ) -> LipaResult<()> {
+        match wallet.sync(blockchain, SyncOptions::default()) {
+            Ok(_) => {}
+            Err(e) => {
+                return match e {
+                    bdk::Error::Electrum(e) => Err(runtime_error(ElectrumServiceUnavailable, e)),
+                    bdk::Error::Sled(e) => Err(permanent_failure(e)),
+                    _ => Err(runtime_error(GenericError, "Failed to sync the BDK wallet")),
+                }
+            }
+        };
+        Ok(())
+    }
 }
 
 #[cfg(test)]
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,4 +1,7 @@
-use bdk::bitcoin::Network;
+use bdk::bitcoin::consensus::deserialize;
+use bdk::bitcoin::psbt::PartiallySignedTransaction;
+use bdk::bitcoin::{Address, Network, Txid};
+use std::str::FromStr;
 use uniffi_lipabusinesslib::{Config, Wallet};
 
 const WATCH_DESCRIPTOR: &str = "wpkh([aed2a027/84'/1'/0']tpubDCvyR4gGk5U6r1Q1HMQtgZYMD3a9bVyt7Tv9BWgcBCQsff4aqR7arUGPTMaUbVwaH8TeaK924GJr9nHyGPBtqSCD8BCjMnJb1qZFjK4ACfL/0/*)";
@@ -16,4 +19,40 @@ fn test_get_balance_testnet_electrum() {
     let balance = wallet.sync_balance().unwrap();
 
     assert_eq!(balance.confirmed, 88009);
+}
+
+const TESTNET_ADDR: &str = "tb1q3ctet25lk00cmvrtkmu9dmah2kj077m4n4aqtm";
+
+#[test]
+fn test_drain_wallet() {
+    let wallet = Wallet::new(Config {
+        electrum_url: "ssl://electrum.blockstream.info:60002".to_string(),
+        wallet_db_path: ".bdk-database2".to_string(),
+        network: Network::Testnet,
+        watch_descriptor: WATCH_DESCRIPTOR.to_string(),
+    })
+    .unwrap();
+
+    let drain_tx = wallet
+        .prepare_drain_tx(TESTNET_ADDR.to_string(), 1)
+        .unwrap();
+
+    assert_eq!(drain_tx.output_sat + drain_tx.on_chain_fee_sat, 88009);
+
+    let psbt = deserialize::<PartiallySignedTransaction>(&drain_tx.blob).unwrap();
+
+    assert_eq!(
+        psbt.unsigned_tx.txid(),
+        Txid::from_str(&drain_tx.id).unwrap()
+    );
+
+    assert_eq!(psbt.unsigned_tx.output.len(), 1);
+    assert_eq!(
+        psbt.unsigned_tx.output.get(0).unwrap().value,
+        drain_tx.output_sat
+    );
+    assert_eq!(
+        psbt.unsigned_tx.output.get(0).unwrap().script_pubkey,
+        Address::from_str(TESTNET_ADDR).unwrap().script_pubkey()
+    );
 }
